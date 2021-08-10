6edd1247b4a5a700887dd43dfaa514b65216972b
Luke Gill <bagrad007@gmail.com>
2021-08-10 18:34:18 -0400

    adds folder for back end and sets up basics for start of server

---
+++ back-end/server.js
@@ -0,0 +1,23 @@
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+
+const app = express()
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = "mongodb+srv://bagrad:bagrad123@cluster0.ezb6f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const PORT = process.env.port || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
