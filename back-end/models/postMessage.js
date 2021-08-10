9334da1f94336d1ab04a68211a2b5f1d05128313
Luke Gill <bagrad007@gmail.com>
2021-08-10 19:10:51 -0400

    adds controllers folder for posts, adds schema for posts, and models folder for postMessages. Also adds routes folder for routes

---
+++ back-end/models/postMessage.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema({
+    selectedFile: String,
+    comment: String,
+    tags: [String],
+    creator: String,
+    cookieCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+
+})
+
+const PostMessage = mongoose.model('PostMessage', postSchema)
+export default PostMessage
