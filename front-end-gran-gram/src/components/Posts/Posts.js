2cf17faa5a6b245ecb2e488d6355f7f1872765e7
Luke Gill <bagrad007@gmail.com>
2021-08-13 17:26:39 -0400

    fixes spinner and adds grandma picture to loading screen

--- front-end-gran-gram/src/components/Posts/Posts.js
+++ front-end-gran-gram/src/components/Posts/Posts.js
@@ -7,12 +7,26 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Spinner from 'react-bootstrap/Spinner'
+import Card from 'react-bootstrap/Card'
 
 const Posts = () => {
     const posts = useSelector((state) => state.posts)
     console.log(posts)
-    return (
-        !posts.length ? <Spinner /> : (
+    if (!posts.length) {
+        return (
+            <Container>
+                <Row>
+                    <Col className="offset-lg-4">
+                        <Card style={{ width: '22rem' }}>
+                            <Card.Img src="https://c8.alamy.com/comp/A5YY44/elderly-lady-isolating-covid-19-computer-internet-senior-old-age-lady-A5YY44.jpg" />
+                        </Card>
+                        <Spinner animation="border" className='offset-sm-3' />
+                    </Col>
+                </Row>
+            </Container>
+        )
+    } else {
+        return (
             <Container>
                 <Row>
                     <Col lg={6} className="shadow-lg bg-white offset-lg-3">
@@ -25,7 +39,9 @@ const Posts = () => {
                 </Row>
             </Container>
         )
-    )
+    }
+
+
 }
 
 export default Posts
