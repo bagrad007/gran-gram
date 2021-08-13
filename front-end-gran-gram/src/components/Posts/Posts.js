341ef5ffa06a2ed07804f5de9a5299ec59e52554
Luke Gill <bagrad007@gmail.com>
2021-08-13 13:25:57 -0400

    starts adding feed background color and adds bootstrap elements to feed, such as container, row, and col

--- front-end-gran-gram/src/components/Posts/Posts.js
+++ front-end-gran-gram/src/components/Posts/Posts.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Spinner from 'react-bootstrap/Spinner'
+
 const Posts = () => {
     const posts = useSelector((state) => state.posts)
     console.log(posts)
     return (
-        <div className="container">
-            <h1>Feed</h1>
-        </div>
+
+        <Container>
+            <Row>
+                <Col lg={6} className="shadow-lg bg-white offset-lg-3">
+                    Feed
+                </Col>
+            </Row>
+        </Container>
+
     )
 }
 
