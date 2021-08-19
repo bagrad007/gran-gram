3e033af8117b82044e99a8cf0e59ae7de07813cd
Luke Gill <bagrad007@gmail.com>
2021-08-19 14:11:48 -0400

    fixes display on navbar when logged in or out with google

--- front-end-gran-gram/src/components/NavBar/Navigation.js
+++ front-end-gran-gram/src/components/NavBar/Navigation.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 import BrandLogo from '../../images/GranGram.png'
 
@@ -8,17 +9,19 @@ import Nav from 'react-bootstrap/Nav'
 
 
 const Navigation = () => {
+    const dispatch = useDispatch()
+    const location = useLocation()
 
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
 
     useEffect(() => {
         // const token = user.token
 
-
         setUser(JSON.parse(localStorage.getItem('profile')))
-    }, [])
+    }, [location])
 
     const history = useHistory()
+
     const handleNewPost = (e) => {
         e.preventDefault()
         history.push('/post/new')
@@ -33,21 +36,42 @@ const Navigation = () => {
         history.push('/login')
     }
 
+    const handleLogout = (e) => {
+        dispatch({ type: "LOGOUT" })
+        history.push('/')
+
+        setUser(null)
+    }
+    console.log(user)
+
     return (
         <div>
             <Navbar bg="light" expand="lg" sticky="top">
                 <Navbar.Brand><img src={BrandLogo} alt='Brand' height="50" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/" onClick={handleHome}>Home</Nav.Link>
-                        <Nav.Link href='/post/new' onClick={handleNewPost}>Create Post</Nav.Link>
-                    </Nav>
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/login" onClick={handleLogin} >Login</Nav.Link>
+                {user !== null ? (
+                    <Navbar.Collapse id="basic-navbar-nav">
 
-                    </Nav>
-                </Navbar.Collapse>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/" onClick={handleHome}>Home</Nav.Link>
+                            <Nav.Link href='/post/new' onClick={handleNewPost}>Create Post</Nav.Link>
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/logout" onClick={handleLogout}>Logout</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                ) : (
+                    <Navbar.Collapse>
+                        <Nav className="me-auto">
+
+                            <Nav.Link href="/" onClick={handleHome}>Home</Nav.Link>
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/login" onClick={handleLogin} >Login</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                )}
             </Navbar>
 
         </div>
